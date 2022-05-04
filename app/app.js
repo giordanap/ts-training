cf0d7bfce6fb67f0217155944c0614e2cc10df76
Giordan Arredondo <garredondop@uni.pe>
2022-05-04 15:10:46 -0500

    delete fields

--- app/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("Hola Patricia!");
-let lista = ["Gius", "Gabriel", "Greta"];
-for (const iterator of lista) {
-    console.log(iterator);
-}
--- app/app.ts
+++
@@ -1,7 +0,0 @@
-console.log("Hola Patricia!");
-
-let lista:string[] = ["Gius", "Gabriel", "Greta"];
-
-for (const iterator of lista) {
-    console.log(iterator);
-}
