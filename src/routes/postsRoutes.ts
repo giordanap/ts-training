350241d3ed3ea08551b25e4938ca049d84a3d4a0
Giordan Arredondo <garredondop@uni.pe>
2022-05-06 10:21:12 -0500

    Post Routes CRUD

--- src/routes/postsRoutes.ts
+++ src/routes/postsRoutes.ts
@@ -11,26 +11,33 @@ class PostRoutes {
         this.routes();
     }
 
-    async getPosts(req: Request, res: Response ){
+    public async getPosts(req: Request, res: Response ):Promise<void> {
         const posts = await Post.find();
         res.json(posts);
     }
     
-    getPost(){
-
+    public async getPost(req: Request, res: Response):Promise<void> {
+        const post = await Post.findOne({url: req.params.url})
+        res.json(post);
     }
     
-    createPost(req: Request, res: Response){
-        console.log(req.body);
-        res.json('Received!')
+    public async createPost(req: Request, res: Response):Promise<void> {
+        const { title, url, content, image } = req.body;
+        const newPost = new Post({ title, url, content, image });
+        await newPost.save();
+        res.json({ data: newPost });
     }
 
-    updatePost(){
-
+    public async updatePost(req: Request, res: Response):Promise<void> {
+        const { url } = req.params;
+        const post = await Post.findOneAndUpdate({ url }, req.body, {new: true});
+        res.json(post);
     }
 
-    deletePost(){
-
+    public async deletePost(req: Request, res: Response):Promise<void> {
+        const { url } = req.params;
+        await Post.findOneAndDelete({url});
+        res.json({response: 'Post deleted successfully'});
     }
 
     routes(){
